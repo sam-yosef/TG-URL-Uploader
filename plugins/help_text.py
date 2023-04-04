4a68e955486eabbc5531a6d2adc3896a544824e0
sam-yosef <119949665+sam-yosef@users.noreply.github.com>
2023-04-04 05:36:53 +0300

    Add files via upload

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -59,15 +59,15 @@ async def get_me_info(bot, update):
     )
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
+@pyrogram.Client.on_message(pyrogram.filters.command(["start"]))
 async def start(bot, update):
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/start")
-await bot.send_message(
-    chat_id=update.chat.id,
-    text=Translation.START_TEXT,
-    reply_to_message_id=update.message.message_id
-)
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.START_TEXT,
+        reply_to_message_id=update.message_id
+    )
 
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
