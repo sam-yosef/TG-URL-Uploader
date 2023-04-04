b3f2e3da408d35aa8879124d8e7018de5dce6eee
sam-yosef <119949665+sam-yosef@users.noreply.github.com>
2023-04-04 05:51:54 +0300

    Add files via upload

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -66,7 +66,7 @@ async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT,
-        reply_to_message_id=update.message.message_id
+        reply_to_message_id=update.message_id
     )
 
 
