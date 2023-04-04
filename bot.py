1338ada7abaa3a58113fbfc8377979f5e3373195
sam-yosef <119949665+sam-yosef@users.noreply.github.com>
2023-04-04 04:29:00 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,47 +3,56 @@ import logging
 import os
 
 from aiohttp import web
+import pyrogram
+assert pyrogram
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-# the secret configuration specific things
+# The secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
 
-import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+# Create download directory, if not exist
+if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+    os.makedirs(Config.DOWNLOAD_LOCATION)
 
+plugins = dict(
+    root="plugins"
+)
+
+app = pyrogram.Client(
+    "AnyDLBot",
+    bot_token=Config.TG_BOT_TOKEN,
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+    plugins=plugins
+)
+Config.AUTH_USERS.add(784291834)
+
+# Here we begin the aiohttp server
 async def handle(request):
-    return web.Response(text="Hello, world!")
+    return web.Response(text="Hello, world")
 
+app.router.add_get('/', handle)
 
 if __name__ == "__main__":
-    # create download directory, if not exist
-    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
-        os.makedirs(Config.DOWNLOAD_LOCATION)
-    plugins = dict(
-        root="plugins"
-    )
-    app = pyrogram.Client(
-        "AnyDLBot",
-        bot_token=Config.TG_BOT_TOKEN,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins
-    )
-    Config.AUTH_USERS.add(784291834)
+    try:
+        import nest_asyncio
 
-    # Adding aiohttp webserver
-    web_app = web.Application()
-    web_app.router.add_get('/', handle)
+        nest_asyncio.apply()
+    except ImportError:
+        pass
 
-    server = web.AppRunner(web_app)
-    app.loop.run_until_complete(server.setup())
-    web.run_app(web_app, host='localhost', port=8080)
+    import asyncio
 
-    app.run()
+    asyncio.ensure_future(app.start())
+
+    web.run_app(app, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+
+    # This is the closing
+    app.stop()
