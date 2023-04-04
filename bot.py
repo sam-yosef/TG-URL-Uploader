71d404c67fa5879b8a9e7fc9df2057fb2d467fc2
sam-yosef <119949665+sam-yosef@users.noreply.github.com>
2023-04-04 04:00:31 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,14 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
 
-# the logging things
 import logging
+import os
+
+from aiohttp import web
+
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
-import os
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
@@ -20,7 +18,11 @@ import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-if __name__ == "__main__" :
+async def handle(request):
+    return web.Response(text="Hello, world!")
+
+
+if __name__ == "__main__":
     # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
@@ -34,5 +36,14 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(683538773)
+    Config.AUTH_USERS.add(784291834)
+
+    # Adding aiohttp webserver
+    web_app = web.Application()
+    web_app.router.add_get('/', handle)
+
+    server = web.AppRunner(web_app)
+    app.loop.run_until_complete(server.setup())
+    web.run_app(web_app, host='localhost', port=8080)
+
     app.run()
